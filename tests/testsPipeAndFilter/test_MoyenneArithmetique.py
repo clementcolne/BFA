e8a4285416524aed7ed8e62391cfba7630b936eb
Mireille <32102096+MireilleVignal@users.noreply.github.com>
2021-05-08 16:03:21 +0200

    Ajout d'un fichier README.md, séparation des tests par configuration de graph

--- tests/testsPipeAndFilter/test_MoyenneArithmetique.py
+++ tests/testsPipeAndFilter/test_MoyenneArithmetique.py
@@ -5,34 +5,35 @@ from tools.ConfigTest import ConfigTest
 
 
 class TestMoyenneArithmetique(TestCase):
-    def test_process(self):
+    def test_process_hausse(self):
         # Création des objets pour les tests
         action1 = Action("Test1")
-        action2 = Action("Test2")
-        action3 = Action("Test3")
         ma = MoyenneArithmetique()
 
-        # Test tendance haussiere
+        # Test
         action1.remplirGraph(ConfigTest.get_graph_hausse())
-
         ma.process(action1)
         action1.calculFinalNote()
 
         self.assertTrue(action1.getFinalNote() > 10)
 
-        # Test tendance baissiere
-        action2.remplirGraph(ConfigTest.get_graph_baisse())
+    def test_process_baisse(self):
+        action1 = Action("Test1")
+        ma = MoyenneArithmetique()
 
-        ma.process(action2)
-        action2.calculFinalNote()
+        action1.remplirGraph(ConfigTest.get_graph_baisse())
+        ma.process(action1)
+        action1.calculFinalNote()
 
-        self.assertTrue(action2.getFinalNote() < 10)
+        self.assertTrue(action1.getFinalNote() < 10)
 
-        # Test graph stable
-        action3.remplirGraph(ConfigTest.get_graph_stable())
+    def test_process_stable(self):
+        action1 = Action("Test1")
+        ma = MoyenneArithmetique()
 
-        ma.process(action3)
-        action3.calculFinalNote()
+        action1.remplirGraph(ConfigTest.get_graph_stable())
+        ma.process(action1)
+        action1.calculFinalNote()
 
-        #Resultat faux lorsque le graph se construit en hausse ou en baisse
-        self.assertTrue(40 > action3.getFinalNote() > -40)
+        # Resultat faux lorsque le graph se construit en hausse ou en baisse
+        self.assertTrue(40 > action1.getFinalNote() > -40)
--- tests/testsPipeAndFilter/test_Resistance.py
+++ tests/testsPipeAndFilter/test_Resistance.py
@@ -5,11 +5,9 @@ from tools.ConfigTest import ConfigTest
 
 
 class TestResistance(TestCase):
-    def test_process(self):
+    def test_process_hausse(self):
         # Création des objets pour les tests
         actionTest1 = Action("Test1")
-        actionTest2 = Action("Test2")
-        actionTest3 = Action("Test3")
         res = Resistance()
 
         actionTest1.remplirGraph(ConfigTest.get_graph_hausse())
@@ -20,16 +18,22 @@ class TestResistance(TestCase):
 
         self.assertEqual(actionTest1.getFinalNote(), 10)
 
-        # Test 2
-        actionTest2.remplirGraph(ConfigTest.get_graph_baisse())
-        res.process(actionTest2)
-        actionTest2.calculFinalNote()
+    def test_process_baisse(self):
+        actionTest1 = Action("Test1")
+        res = Resistance()
 
-        self.assertEqual(actionTest2.getFinalNote(), -10)
+        actionTest1.remplirGraph(ConfigTest.get_graph_baisse())
+        res.process(actionTest1)
+        actionTest1.calculFinalNote()
 
-        # Test 3
-        actionTest3.remplirGraph(ConfigTest.get_graph_stable())
-        res.process(actionTest3)
-        actionTest3.calculFinalNote()
+        self.assertEqual(actionTest1.getFinalNote(), -10)
 
-        self.assertEqual(actionTest3.getFinalNote(), 0)
+    def test_process_stable(self):
+        actionTest1 = Action("Test1")
+        res = Resistance()
+
+        actionTest1.remplirGraph(ConfigTest.get_graph_stable())
+        res.process(actionTest1)
+        actionTest1.calculFinalNote()
+
+        self.assertEqual(actionTest1.getFinalNote(), 0)
